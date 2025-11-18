758d9a5b4a964776bc154b02661eebb4c383a43a
Koushik Karmakar <koushik9339mail@gmail.com>
2025-11-18 16:57:29 +0530

    Update connection.js

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 export async function databaseConnection() {
- mongoose.connect(process.env.MONGO_URL)
+return mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("DB Error: ", err));
 }
